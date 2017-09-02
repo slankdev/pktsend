db7c5a84fc704c530b086f3544388857d59b5d33
Hiroki SHIROKURA <slank.dev@gmail.com>
2017-09-02 19:17:08 +0900

    Imple prototype

--- main.cc
+++ main.cc
@@ -2,7 +2,7 @@
 /*
  * MIT License
  *
- * Copyright (c) 2017 Susanow
+ * Copyright (c) 2017 Project Susanow
  * Copyright (c) 2017 Hiroki SHIROKURA
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
@@ -34,15 +34,23 @@
 #include <slankdev/checksum.h>
 #include <slankdev/color.h>
 #include <pgen/io.h>
+#include <pgen/core.h>
 using namespace slankdev;
 
 std::string ifname = "";
 std::string filename = "";
 size_t count = 1;
+bool verbose = false;
+bool hex = false;
 
-std::string str_hdst  = "";
-std::string str_hsrc  = "";
-std::string str_etype = "";
+std::string str_hdst    = "";
+std::string str_hsrc    = "";
+std::string str_etype   = "";
+std::string str_tot_len = "";
+std::string str_proto   = "";
+std::string str_psrc    = "";
+std::string str_pdst    = "";
+std::string str_csum    = "";
 
 void log(const char* fmt, ...)
 {
@@ -69,36 +77,46 @@ void usage(const char* progname)
   printf("    -i ifname                    interface name  \n");
   printf("    -w file                      write as pcap format\n");
   printf("    -c count                     packet count    \n");
-  printf("    -v                           show version    \n");
   printf("    -h                           show usage    \n");
+  printf("    --version                    show version    \n");
+  printf("    --verbose                    verbose output  \n");
+  printf("    --hex                        print packet as hex \n");
 
   printf("\n");
   printf("Option for Crafting Packet Binary\n");
   printf("    --hsrc=11:22:33:44:55:66     src mac address \n");
   printf("    --hdst=ff:ff:ff:ff:ff:ff     dst mac address \n");
   printf("    --etype=0x0800               ethernet type   \n");
-  // printf("    --psrc=192.168.0.10          src ip address  \n");
-  // printf("    --pdst=192.168.0.1           dst ip address  \n");
-  // printf("    --proto=1                    ip protocol     \n");
+  printf("    --psrc=192.168.0.10          src ip address  \n");
+  printf("    --pdst=192.168.0.1           dst ip address  \n");
+  printf("    --proto=1                    ip protocol     \n");
 
   printf("\n");
 }
 
 inline static size_t
-craft_eh(struct ether* eh, uint8_t dst[], uint8_t src[], uint16_t type)
+craft_eh(struct ether* eh,
+    pgen::macaddress& dst, pgen::macaddress& src, uint16_t type)
 {
   constexpr size_t IP_ADDR_LEN = 6;
   for (size_t i=0; i<IP_ADDR_LEN; i++) {
-    eh->dst.addr_bytes[i] = dst[i];
-    eh->src.addr_bytes[i] = src[i];
+    eh->dst.addr_bytes[i] = dst.get_octet(i+1);
+    eh->src.addr_bytes[i] = src.get_octet(i+1);
   }
   eh->type = bswap16(type);
   return sizeof(ether);
 }
 
 inline static size_t
-craft_ih(struct ip* ih, uint16_t tot_len, uint8_t proto, int32_t csum, uint32_t src, uint32_t dst)
+craft_ih(struct ip* ih, uint16_t tot_len, uint8_t proto, int32_t csum,
+    pgen::ipv4address& src, pgen::ipv4address& dst)
 {
+  union U {
+    uint32_t u32;
+    uint8_t u8[4];
+  };
+  U src_U, dst_U;
+
   ih->ver_ihl = 0x45;
   ih->tos     = 0x00;
   ih->tot_len = bswap16(tot_len);
@@ -107,8 +125,13 @@ craft_ih(struct ip* ih, uint16_t tot_len, uint8_t proto, int32_t csum, uint32_t 
   ih->ttl     = 0x40;
   ih->proto   = proto;
   ih->sum     = bswap16(0x0000);
-  ih->src     = bswap32(src);
-  ih->dst     = bswap32(dst);
+
+  for (size_t i=0; i<4; i++) {
+    src_U.u8[i] = src.get_octet(i+1);
+    dst_U.u8[i] = dst.get_octet(i+1);
+  }
+  ih->src     = src_U.u32;
+  ih->dst     = dst_U.u32;
 
   if (csum < 0) {
     ih->sum = bswap16(checksum(ih, 20));
@@ -121,11 +144,40 @@ craft_ih(struct ip* ih, uint16_t tot_len, uint8_t proto, int32_t csum, uint32_t 
 inline static void
 dump_packet(const ether* eh, const ip* ih, uint8_t* pkt_ptr, size_t pkt_len)
 {
-  log("Dump Packet Information\n\n");
   eh->print(stdout);
   ih->print(stdout);
-  slankdev::hexdump(stdout, pkt_ptr, pkt_len);
-  printf("\n\n");
+}
+
+enum OPT_FLAG {
+  F_HDST=1,
+  F_HSRC ,
+  F_ETYPE,
+  F_TOT_LEN,
+  F_PROTO,
+  F_CSUM,
+  F_PSRC,
+  F_PDST,
+  F_VERBOSE,
+  F_VERSION,
+  F_HEX,
+};
+
+inline static void
+parse_long_opt(OPT_FLAG flag, const char* optarg)
+{
+  switch (flag) {
+    case F_ETYPE  : str_etype   = optarg; break;
+    case F_HSRC   : str_hsrc    = optarg; break;
+    case F_HDST   : str_hdst    = optarg; break;
+    case F_TOT_LEN: str_tot_len = optarg; break;
+    case F_PROTO  : str_proto   = optarg; break;
+    case F_CSUM   : str_csum    = optarg; break;
+    case F_PSRC   : str_psrc    = optarg; break;
+    case F_PDST   : str_pdst    = optarg; break;
+    case F_VERBOSE: verbose = true      ; break;
+    case F_HEX    : hex     = true      ; break;
+    case F_VERSION: version(); exit(0);
+  }
 }
 
 inline static void
@@ -133,19 +185,20 @@ parse_opt(int argc, char** argv)
 {
   int optflag = -1;
 
-  enum F {
-    F_HDST=1,
-    F_HSRC ,
-    F_ETYPE,
-  };
   static struct option long_options[] =
   {
     /* These options don’t set a flag. We distinguish them by their indices. */
-    {"hdst" ,  required_argument, &optflag, F_HDST},
-    {"hsrc" ,  required_argument, &optflag, F_HSRC},
-    {"etype",  required_argument, &optflag, F_ETYPE},
-    // {"create",  required_argument, 0, 'c'},
-    // {"file",    required_argument, 0, 'f'},
+    {"hdst"   ,  required_argument, &optflag, F_HDST},
+    {"hsrc"   ,  required_argument, &optflag, F_HSRC},
+    {"etype"  ,  required_argument, &optflag, F_ETYPE},
+    {"tot_len",  required_argument, &optflag, F_TOT_LEN},
+    {"proto"  ,  required_argument, &optflag, F_PROTO},
+    {"csum"   ,  required_argument, &optflag, F_CSUM},
+    {"psrc"   ,  required_argument, &optflag, F_PSRC},
+    {"pdst"   ,  required_argument, &optflag, F_PDST},
+    {"verbose",  no_argument      , &optflag, F_VERBOSE},
+    {"version",  no_argument      , &optflag, F_VERSION},
+    {"hex"    ,  no_argument      , &optflag, F_HEX},
     {0, 0, 0, 0},
   };
 
@@ -156,9 +209,7 @@ parse_opt(int argc, char** argv)
     if (c == -1) break;
     switch (c) {
       case 0: {
-        const struct option* lo = &long_options[option_index];
-        printf("LONGOPT:  optarg=%s flag=%d\n", optarg, optflag);
-        // throw slankdev::exception("Not Support This Option yet");
+        parse_long_opt((OPT_FLAG)optflag, optarg);
         break;
       }
       case 'c':
@@ -170,9 +221,6 @@ parse_opt(int argc, char** argv)
       case 'w':
         filename = optarg;
         break;
-      case 'v':
-        version();
-        exit(0);
       case 'h':
         usage(argv[0]);
         exit(0);
@@ -183,6 +231,12 @@ parse_opt(int argc, char** argv)
   }
 }
 
+uint32_t htoi(const char* str)
+{
+  char *endptr;
+  uint32_t n = strtol(str, &endptr, 16);
+  return n;
+}
 
 int main(int argc, char** argv)
 {
@@ -196,20 +250,24 @@ int main(int argc, char** argv)
    * Craft Ethernet Header
    */
   ether* eh = (ether*)(pkt_ptr);
-  uint8_t dst_mac[] = {0xff, 0xff, 0xff, 0xff, 0xff, 0xff};
-  uint8_t src_mac[] = {0x11, 0x22, 0x33, 0x44, 0x55, 0x66};
-  uint16_t type = 0x0800;
+  pgen::macaddress dst_mac;
+  pgen::macaddress src_mac;
+  uint16_t type = str_etype==""?0x0800:htoi(str_etype.c_str());
+  dst_mac = str_hdst==""?"ff:ff:ff:ff:ff:ff":str_hdst;
+  src_mac = str_hsrc==""?"ff:ff:ff:ff:ff:ff":str_hsrc;
   pkt_len += craft_eh(eh, dst_mac, src_mac, type);
 
   /*
    * Craft IPv4 Header
    */
   ip* ih = (ip*)(pkt_ptr + pkt_len);
-  uint16_t tot_len = 20 + strlen(str);
-  uint8_t  proto = 0x01;
-  int32_t  csum = -1;
-  uint32_t src_ip = 0xc0a8000a;
-  uint32_t dst_ip = 0xc0a80001;
+  uint16_t tot_len = str_tot_len==""?(20+strlen(str)):atoi(str_tot_len.c_str());
+  uint8_t  proto = str_proto==""?(0x01):atoi(str_proto.c_str());
+  int32_t  csum = str_csum==""?(-1):htoi(str_csum.c_str());
+  pgen::ipv4address src_ip;
+  pgen::ipv4address dst_ip;
+  src_ip = str_psrc==""?"192.168.0.10":str_psrc;
+  dst_ip = str_pdst==""?"192.168.0.1" :str_pdst;
   pkt_len += craft_ih(ih, tot_len, proto, csum, src_ip, dst_ip);
 
   /*
@@ -222,7 +280,8 @@ int main(int argc, char** argv)
   /*
    * Dump Packet information
    */
-  dump_packet(eh, ih, pkt_ptr, pkt_len);
+  if (verbose) dump_packet(eh, ih, pkt_ptr, pkt_len);
+  if (hex)     slankdev::hexdump(stdout, pkt_ptr, pkt_len);
 
   /*
    * Send Network Interface if ifnams is set;
